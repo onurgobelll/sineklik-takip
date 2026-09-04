79105e9d35b96aac2187e4bbd5f3fd0925eedfb5
onurgobelll <onurgobelll@gmail.com>
2026-09-04 22:42:55 +0300

    Update ayar.js

--- ayar.js
+++ ayar.js
@@ -4,7 +4,7 @@
 
 // Firebase Console > SINEKLIK > Proje ayarları > Genel > "Sineklik Takip Web"
 // kartındaki apiKey değerini tırnakların arasına yapıştır:
-window.SINEKLIK_ANAHTAR = "";
+window.SINEKLIK_ANAHTAR = "AIzaSyAvFrR3fLxYenP603ujutD_TLNiRNmO_Aw";
 
 // Realtime Database adresi (değiştirmen gerekmiyor):
 window.SINEKLIK_DB = "https://sineklik-ac0bc-default-rtdb.firebaseio.com";
